36412d36fe62200a371c72f8fb00ed63b40d8a44
Lại Quang Khải <104859097+khailq0909@users.noreply.github.com>
2024-04-26 16:37:12 +0700

    auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,13 +2,10 @@ import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 import { createError } from "../utils/error.js";
 import dotenv from "dotenv";
 dotenv.config();
-
-
-
 
 const transporter = nodemailer.createTransport({
   service: "Gmail",
@@ -16,9 +13,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
-  tls:{
-    rejectUnauthorized: false
-  }
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const register = async (req, res, next) => {
@@ -27,7 +24,7 @@ export const register = async (req, res, next) => {
     const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({
       ...req.body,
-      emailToken: crypto.randomBytes(64).toString('hex'),
+      emailToken: crypto.randomBytes(64).toString("hex"),
       password: hash,
     });
     await newUser.save();
@@ -38,19 +35,19 @@ export const register = async (req, res, next) => {
       },
       to: newUser.email,
       subject: "Verify your email",
-      html:`<h2>${newUser.lastname}! Thanks for joining with MyHomeStay.com</h2>
+      html: `<h2>${newUser.lastname}! Thanks for joining with MyHomeStay.com</h2>
       <h4>Please verify your email to continue...</h4>
-      <a href="https://finalproject-api.onrender.com/api/users/verify/${newUser.emailToken}">Click here to verify</a>
-      `
+      <a href="http://localhost:3001/api/users/verify/${newUser.emailToken}">Click here to verify</a>
+      `,
     };
-    transporter.sendMail(mailOptions,(err,info)=>{
-      if(err){
-        console.log(err)
-      }else{
-        console.log('Verifycation email is sent to your gmail account')
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Verifycation email is sent to your gmail account");
       }
     });
-    
+
     res.status(200).send("User has been created.");
   } catch (err) {
     next(err);
@@ -65,29 +62,47 @@ export const login = async (req, res, next) => {
       req.body.password,
       user.password
     );
-    if(user.isActive === false){
-    return next(createError(400, "Your account has been baned!"));
+    if (user.isActive === false) {
+      return next(createError(400, "Your account has been baned!"));
     }
-    if(user.isVerified === false) return next(createError(400, "Your email is not verified. Please check your email !!!"))
+    if (user.isVerified === false)
+      return next(
+        createError(
+          400,
+          "Your email is not verified. Please check your email !!!"
+        )
+      );
     if (!isPasswordCorrect)
       return next(createError(400, "Wrong password or username!"));
-    
+
     const token = jwt.sign(
-      { userId: user._id, isAdmin: user.isAdmin, isHomeOwner: user.isHomeOwner},
-      process.env.JWT_SECRET_KEY,
+      {
+        userId: user._id,
+        isAdmin: user.isAdmin,
+        isHomeOwner: user.isHomeOwner,
+      },
+      process.env.JWT_SECRET_KEY
     );
 
-    const {password,phone,birthday,city,createdAt,gender,updatedAt, ...otherDetails } = user._doc;
+    const {
+      password,
+      phone,
+      birthday,
+      city,
+      createdAt,
+      gender,
+      updatedAt,
+      ...otherDetails
+    } = user._doc;
     res
       .cookie("access_token", token, {
         httpOnly: true,
         maxAge: 86400000,
-        secure: true
       })
       .status(200)
-      .json({ details: { ...otherDetails }});
+      .json({ details: { ...otherDetails } });
   } catch (err) {
-    next(err);  
+    next(err);
   }
 };
 
@@ -98,14 +113,17 @@ export const logout = async (req, res) => {
   res.send();
 };
 
-
 export const sendCode = async (req, res) => {
-  const email = req.body.email
-  const Otp = `${Math.floor(1000 + Math.random() * 9000)}`
-  try{
-    const user = await User.findOneAndUpdate({email: email},{otpToken:Otp},{new:true})
+  const email = req.body.email;
+  const Otp = `${Math.floor(1000 + Math.random() * 9000)}`;
+  try {
+    const user = await User.findOneAndUpdate(
+      { email: email },
+      { otpToken: Otp },
+      { new: true }
+    );
     if (!user) {
-      return res.status(404).json({message:"Email not found"})
+      return res.status(404).json({ message: "Email not found" });
     }
     const mailOptions = {
       from: {
@@ -114,49 +132,71 @@ export const sendCode = async (req, res) => {
       },
       to: user.email,
       subject: `Forgot Password code ${user.otpToken}`,
-      html:`<h2>MyHomeStay.com Secret Verification code</h2>
+      html: `<h2>MyHomeStay.com Secret Verification code</h2>
       <p>Dear ${user?.lastname}</p>
       <p>This is your code to change your password</p>
       <p>Please never share this code for anyone. Thank you for using our services</p>
-      `
+      `,
     };
-    transporter.sendMail(mailOptions,(err,info)=>{
-      if(err){
-        console.log(err)
-      }else{
-        console.log('Verifycation code is sent to your gmail account')
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Verifycation code is sent to your gmail account");
       }
     });
-    res.status(200).json({ message: "Vertification code is sent to your gmail account"})
-  }catch(err){
+    res
+      .status(200)
+      .json({ message: "Vertification code is sent to your gmail account" });
+  } catch (err) {
     return next(createError(400, "Some thing went wrong"));
   }
 };
-export const checkOTPCode = async (req,res,next)=>{
+export const checkOTPCode = async (req, res, next) => {
   const otp = req.body.otp;
-  const user = await User.findOne({otpToken: otp})
-  if(user){
-    res.status(200).json({ message:"Otp code is valid"})
-  }else{
+  const user = await User.findOne({ otpToken: otp });
+  if (user) {
+    res.status(200).json({ message: "Otp code is valid" });
+  } else {
     return next(createError(400, "OTP is not valid!"));
   }
+};
+export const checkPassWord = async (req, res, next) => {
+  const salt = bcrypt.genSaltSync(10);
+  const email = req.body.email;
+  const user = await User.findOne({ email: email });
+  console.log(user)
+  const currentPass = user?.password;
+  const oldPass = req.body.oldPass;
+  const newPass = req.body.newPass;
+  const confirm = req.body.confirmPass;
+  console.log(oldPass, newPass, confirm);
+  try {
+    if (newPass !== confirm) res.status(402).send({ message: "Confirm passwords do not match" });
+    if (bcrypt.compare(currentPass, oldPass)) {
+      let hashPassword = bcrypt.hashSync(newPass, salt);
+      user.password = hashPassword;
+      return res.status(200).send({ message: "Password changed" });
+    } else {
+      res.status(400).send({ message: "Passwords do not match" });
+    }
+  } catch {
+    return next(createError(500, "something went wrong"));
+  }
+};
 
-}
-export const changePassword = async (req,res,next)=>{
-  const email = req.body.email
-console.log(req.body.password)
+export const changePassword = async (req, res, next) => {
+  const email = req.body.email;
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(req.body.password, salt);
   const updateUser = {
-    password:hash,
-    otpToken:null,
+    password: hash,
+    otpToken: null,
+  };
+  try {
+    await User.findOneAndUpdate({ email: email }, updateUser, { new: true });
+    res.status(200).json({ message: "change password successfully" });
+  } catch (err) {
+    next(createError(400, "something went wrong"));
   }
-  try{
-    await User.findOneAndUpdate({email: email},updateUser,{new: true})
-    res.status(200).json({ message:"change password successfully"});
-  }catch(err){
-    next(createError(400, "something went wrong"))
-  }
-}
-
-
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {register, login, logout,sendCode,checkOTPCode,changePassword} from '../controllers/authController.js';
+import {register, login, logout,sendCode,checkOTPCode,changePassword,checkPassWord} from '../controllers/authController.js';
+import { verifyToken } from "../utils/verifytoken.js";
 
 
 const router = express.Router();
@@ -10,6 +11,8 @@ router.post("/logout", logout);
 router.post('/sendcode', sendCode)
 router.post('/checkOtp', checkOTPCode)
 router.post('/changePassword', changePassword)
+router.post('/checkPassword',verifyToken, checkPassWord)
+
 
 
 export default router;
