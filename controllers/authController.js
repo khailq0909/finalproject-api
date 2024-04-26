ced21910ac2c1691f5fa7776ca00d7e8f8304e29
Lại Quang Khải <104859097+khailq0909@users.noreply.github.com>
2024-04-27 00:00:35 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -99,6 +99,7 @@ export const login = async (req, res, next) => {
         httpOnly: true,
         maxAge: 31 * 24 * 60 * 60 * 1000,
         secure: true,
+        sameSite: "none",
       })
       .status(200)
       .json({ details: { ...otherDetails } });
